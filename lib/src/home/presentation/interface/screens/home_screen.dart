eb885335879df70ab5ff70a17fbf9e53b8f641a5
Benedict <benedictasare2@gmail.com>
2024-09-07 11:17:36 +0000

    refactor: add comment to home

--- lib/src/home/presentation/interface/screens/home_screen.dart
+++ lib/src/home/presentation/interface/screens/home_screen.dart
@@ -1,25 +1,31 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:restaurant/shared/constants/app_sizes.dart';
-import 'package:restaurant/shared/constants/app_text.dart';
-import 'package:restaurant/src/home/presentation/interface/widgets/search_bar.dart';
+import 'package:flutter/material.dart'; // Import for Flutter material design components
+import 'package:flutter_riverpod/flutter_riverpod.dart'; // Import for Riverpod state management
+import 'package:restaurant/shared/constants/app_sizes.dart'; // Import for app size constants
+import 'package:restaurant/shared/constants/app_text.dart'; // Import for app text constants
+import 'package:restaurant/src/home/presentation/interface/widgets/search_bar.dart'; // Import for SearchBox widget
 
-import '../../../../../core/restaurant/presentation/riverpod/restaurant_mixin.dart';
-import '../../../../../core/restaurant/presentation/riverpod/restaurant_provider.dart';
-import '../../../../../shared/presentation/theme/extra_colors.dart';
-import '../widgets/error_view.dart';
-import '../widgets/restaurant_list.dart';
+import '../../../../../core/restaurant/presentation/riverpod/restaurant_mixin.dart'; // Import for RestaurantMixin
+import '../../../../../core/restaurant/presentation/riverpod/restaurant_provider.dart'; // Import for Riverpod providers
+import '../../../../../shared/presentation/theme/extra_colors.dart'; // Import for extra colors
+import '../widgets/error_view.dart'; // Import for ErrorView widget
+import '../widgets/restaurant_list.dart'; // Import for RestaurantList widget
 
+// HomeScreen widget, which is a ConsumerWidget with RestaurantMixin functionality
 class HomeScreen extends ConsumerWidget with RestaurantMixin {
   HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // Watch the state of restaurantsProvider
     final restaurantsAsyncValue = ref.watch(restaurantsProvider);
+
+    // Watch the state of filteredRestaurantsProvider
     final filteredRestaurants = ref.watch(filteredRestaurantsProvider);
 
+    // Method to handle search query changes
     void onSearchChanged(String query) {
       if (query.isEmpty) {
+        // If query is empty, show all restaurants
         ref.read(filteredRestaurantsProvider.notifier).state =
             restaurantsAsyncValue.when(
           data: (restaurants) => restaurants,
@@ -27,45 +33,56 @@ class HomeScreen extends ConsumerWidget with RestaurantMixin {
           error: (_, __) => [],
         );
       } else {
+        // Otherwise, filter restaurants based on the search query
         ref.read(filteredRestaurantsProvider.notifier).state =
             searchRestaurants(query, restaurantsAsyncValue.value ?? [], ref);
       }
     }
 
     return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
+      onTap: () => FocusScope.of(context)
+          .unfocus(), // Dismiss keyboard on tap outside input fields
       child: Scaffold(
         appBar: AppBar(
-          title: const AppText.large('Restaurants'),
+          title: const AppText.large('Restaurants'), // Title of the app bar
           actions: [
             IconButton(
-              icon: const Icon(Icons.refresh),
-              onPressed: () => ref.refresh(restaurantsProvider),
+              icon: const Icon(Icons.refresh), // Refresh icon button
+              onPressed: () => ref
+                  .refresh(restaurantsProvider), // Refresh restaurants on press
             ),
           ],
           bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(1.0),
+            preferredSize: const Size.fromHeight(1.0), // Bottom divider height
             child: Divider(
               height: 1.0,
               thickness: 1.0,
-              color: ExtraColors.grey.withOpacity(0.2),
+              color: ExtraColors.grey.withOpacity(0.2), // Divider color
             ),
           ),
         ),
         body: Padding(
-          padding: const EdgeInsets.all(AppSizes.contentPadding),
+          padding: const EdgeInsets.all(
+              AppSizes.contentPadding), // Padding for the body
           child: Column(
             children: [
+              // Search box widget with callback for search changes
               SearchBox(onSearchChanged: onSearchChanged),
-              const SizedBox(height: 16),
+              const SizedBox(height: 16), // Spacer between search box and list
               Expanded(
+                // Expanded widget to make the restaurant list fill available space
                 child: restaurantsAsyncValue.when(
-                  data: (_) => RestaurantList(restaurants: filteredRestaurants),
-                  loading: () =>
-                      const Center(child: CircularProgressIndicator()),
+                  data: (_) => RestaurantList(
+                      restaurants:
+                          filteredRestaurants), // Show restaurant list when data is available
+                  loading: () => const Center(
+                      child:
+                          CircularProgressIndicator()), // Show loading indicator while data is loading
                   error: (error, _) => ErrorView(
-                    error: error.toString(),
-                    onRetry: () => ref.refresh(restaurantsProvider),
+                    error: error
+                        .toString(), // Show error message if an error occurs
+                    onRetry: () => ref.refresh(
+                        restaurantsProvider), // Retry fetching restaurants on press
                   ),
                 ),
               ),
